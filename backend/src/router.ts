587cf7875b11928e7649b84a2f2796406736d920
2WBA123 <wahabadil433@gmail.com>
2023-07-18 14:32:04 +0500

    removed unused files

--- backend/src/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { roleRouter } from './api/role/role.routes';
-import { userRouter } from './api/user/user.routes';
-
-const apiRouter = Router();
-apiRouter.use('/user', userRouter)
-apiRouter.use('/role', roleRouter)
-
-export { apiRouter };
--- backend/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const getEnvironmentPath = (env) =>{
-  return env.NODE_ENV !== 'production' ? `.${env.NODE_ENV || 'development'}` : '';
-};
